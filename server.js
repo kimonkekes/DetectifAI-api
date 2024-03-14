2ea5113ba3eaa61eedc4b8795bd32570b1745152
kimonkekes <kimonkekes1@gmail.com>
2024-03-14 11:02:09 +0200

    Added bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const knex = require('knex')
+const bcrypt = require('bcrypt')
+
+const { handleRegister } = require('./controllers/register') 
+const { handleSignIn } = require('./controllers/signIn')
+const { handleProfile } = require('./controllers/profile')
+const { handleImage } = require('./controllers/image')
 
 const db = knex({
     client: 'pg',
@@ -12,12 +18,12 @@ const db = knex({
     }
   });
 
-db.select('*').from('users')
-
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+const saltRounds = 10;
 
 const database = {
     users: [
@@ -44,54 +50,13 @@ app.get('/', (req, res)=> {
     res.send(database.users)
 })
 
-app.post('/signin', (req, res)=> {
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in')
-    }
-    res.json('signing')
-})
+app.post('/signin', handleSignIn(db, bcrypt))
 
-app.post('/register', (req, res)=> {
-    const { name, email, password } = req.body
-    db('users')
-      .returning('*')
-      .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    })
-      .then(user => {
-        res.json(user[0]) 
-      })
-      .catch(err => res.status(400).json('Unable to register'))
-})
+app.post('/register', handleRegister(db, bcrypt, saltRounds))
 
-app.get('/profile/:id', (req, res)=> {
-    const { id } = req.params
-    db.select('*').from('users').where({id})
-      .then(user =>  {
-        if (user.length) {
-          res.json(user[0])
-        } else {
-          res.status(400).json('User not found')
-        }
-    })
-    .catch(err => res.status(400).json('Error getting user'))
-})
+app.get('/profile/:id', handleProfile(db))
 
-app.put('/image', (req, res)=> {
-    const { id } = req.body
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries)
-    })
-    .catch(err => res.status(400).json('Unable to get entries'))     
-})
+app.put('/image', handleImage(db))
 
 app.listen(3000, ()=> {
     console.log('App is running on port 3000')
