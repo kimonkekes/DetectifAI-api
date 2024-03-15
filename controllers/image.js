4725047bc672e2287c8c16266e6e00ea32fee866
kimonkekes <kimonkekes1@gmail.com>
2024-03-15 12:58:45 +0200

    Moved face recognition to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,48 @@
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const PAT = 'd98f4d87b9cc4ffea0bdad9715cbe3b4'
+const USER_ID = 'kimon'
+const APP_ID = 'image-detection'
+const MODEL_ID = 'face-detection'
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+const handleApiCall = (req, res) => {
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            model_id: MODEL_ID,
+            inputs: [
+                {
+                    data: {
+                        image: {
+                            url: req.body.input,
+                            allow_duplicate_url: true
+                        }
+                    }
+                }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                throw new Error(err);
+            }
+    
+            if (response.status.code !== 10000) {
+                throw new Error("Post model outputs failed, status: " + response.status.description);
+            }
+
+            res.json(response)
+        }
+    )
+}    
+
 const handleImage = (db) => (req, res)=> {
     const { id } = req.body
     db('users').where('id', '=', id)
@@ -9,4 +54,4 @@ const handleImage = (db) => (req, res)=> {
     .catch(err => res.status(400).json('Unable to get entries'))     
 }
 
-module.exports = { handleImage }
+module.exports = { handleImage, handleApiCall }
